a193803dc916f4e132e0751d546f7654149b1d1d
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-20 13:29:14 -0600

    added module, bookCtrl, ng-repeat book in books, call to data folder for guidesjson usng http

--- app/app.js
+++ app/app.js
@@ -7,7 +7,30 @@ module('worldGuide', ['ngRoute'])
       templateUrl: 'guides.html'
     })
 })
-.controller('bookCtrl',function ($scope) {
+.controller('bookCtrl',function ($scope, $http) {
 
   $scope.hello = "HELLOOWWWWWWWW"
-})
+  // $scope.getThedata = factoryname.whatever
+  // .then($scope.data = stuff returned from factory)
+  $http
+    .get('/data/guides.json')
+    .then((response) => {
+      $scope.books = response.data.guides
+      console.log(response)
+      })
+    })
+
+
+
+
+
+
+// Here's a list of some things to include in your app in the appropriate spots:
+
+// Directives:
+// ng-app="<your module name>"
+// ng-controller="bookCtrl"
+// ng-repeat="book in books"
+// A call to the data folder for guides.json using $http
+// Use the $q service to wrap your http call in a promise
+// $scope.books = <your array of guide books data>
